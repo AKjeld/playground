d869239631136f53f93dac3e555f0c99cfe0989c
AKjeld <asger.kjeldsen1@gmail.com>
2024-12-18 00:15:43 +0100

    init d7

---
+++ adventOfCode2024/dec07/dec07.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#// INCLUDES
+#include <fstream>
+#include <unordered_map>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cerr << endl
+         << "finished in " 
+         << clock() * 1.0 / CLOCKS_PER_SEC 
+         << " sec" 
+         << endl;
+    return 0;
+}
